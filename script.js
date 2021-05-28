91dd1c2ed2e75298b37c41e6c069dd0edbac0358
vinicyusx <vinicyus3029@gmail.com>
2021-05-28 19:00:19 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome =("vinicyus");
+let idade=19;
+let ano=2021;
+let resultado= idade - ano;
+console.log=("Meu ano de nascimento e (resultado)");
+let souhumano=[true]
+let number=15;
+let number2=4;
+console.log=("number+number2");
+
